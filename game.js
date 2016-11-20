a7dee6ef924103acbb7c43eb7a8043dfbaeadf71
An Yu <ayu3500@gmail.com>
2016-11-20 00:21:31 -0800

    Draft with canvas

--- game.js
+++ game.js
@@ -14,9 +14,38 @@ The game logic should then check whether the game is over -- if any player has a
 var canvas = document.getElementById("grid");
 var context = canvas.getContext("2d");
 
+var SQUARE_SIZE = 150;
+var BOARD_SIZE = 450;
 
-var gameBoard = function() {
-    this.item = item;
+var board = [];
+
+var newTurn = false;
+
+for (var i = 0; i < 3;i ++) {
+    board[i] = new Array();
+    for (var j = 0; j < 3;j++) {
+        board[i][j] = 0;
+    }
+}
+
+function drawBackground(){
+    for (var x = 0; x <= BOARD_SIZE; x += SQUARE_SIZE) {
+        context.moveTo(x, 0);
+        context.lineTo(x, BOARD_SIZE);
+    }
+    for (var x = 0; x <= BOARD_SIZE; x += SQUARE_SIZE) {
+        context.moveTo(0, x);
+        context.lineTo(BOARD_SIZE, x);
+    }
+    context.strokeStyle = "#EEEEEE";
+    context.lineWidth = 5;
+    context.stroke();
+
+}
+
+document.getElementById('grid').onclick = function() {
+    board[0][0] = 1;
+    var newTurn = true;
 }
 
 function init() {
@@ -25,21 +54,29 @@ function init() {
         draw();
     }
 
-    var draw = function() {        
-       
-    }    
+    var draw = function() {   
+        drawBackground();
+     
+        for (var i = 0; i < 3;i ++) {
+            for (var j = 0; j < 3;j++) {
+                if (board[i][j] == 1) {
+                    context.fillStyle = "#1d1d1d";
+                    context.beginPath();
+                    context.arc(100, 100, 50, 0, Math.PI*2, true);
+                    context.closePath();
+                    context.fill();
+                }
+            }
+        }
+    }
 
 
-
-    function endGame() {
-        clearInterval(gameLoop);
-    };
 
     function newGame() {
         gameLoop = setInterval(game, 350);            
     }
-
     newGame();
+
 }
 
 init();
