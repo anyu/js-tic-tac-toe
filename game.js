f8c8d802740da81474d49f81d6331f768dc6d334
An Yu <ayu3500@gmail.com>
2016-12-04 15:39:10 -0800

    [WIP] XOs alternate, simple styling

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var board = [];
 
-var currentPlayer = 0;
+var currentPlayer = 1;
 
 function init() {
 
@@ -11,25 +11,21 @@ function init() {
         }
     }
 
-    function drawSymbol(row,col, num) {
-        var mark = "";
+    function drawSymbol(row,col, cell_number) {
+        var cell = document.getElementById('square' + cell_number);
+        var mark = document.createElement('div');
+
         if (currentPlayer == 1) {
             board[row][col] = 1;
-            var square = document.getElementById('square' + num);
-            
-            var t = document.createElement('div');
-            t.innerHTML = 'X';
-            square.appendChild(t);
+            mark.className = "symbol x";
+            mark.innerHTML = 'X';
         }
         else {
             board[row][col] = 2;
-            var square = document.getElementById('square' + num);
-
-            var t = document.createElement('div');
-            t.innerHTML = 'O';
-            square.appendChild(t);
+            mark.className = "symbol o";
+            mark.innerHTML = 'O';
         }
-        // draw mark on board
+        cell.appendChild(mark);
         switchPlayer();
     }
 
@@ -65,7 +61,53 @@ function init() {
         }
     }
 
+    document.getElementById('square2').onclick = function() {
+        if (isFree(0,1)) {
+            drawSymbol(0,1,2);
+        }
+    }
 
+    document.getElementById('square3').onclick = function() {
+        if (isFree(0,2)) {
+            drawSymbol(0,2,3);
+        }
+    }
+
+    document.getElementById('square4').onclick = function() {
+        if (isFree(1,0)) {
+            drawSymbol(1,0,4);
+        }
+    }
+
+        document.getElementById('square5').onclick = function() {
+        if (isFree(1,1)) {
+            drawSymbol(1,1,5);
+        }
+    }
+
+    document.getElementById('square6').onclick = function() {
+        if (isFree(1,2)) {
+            drawSymbol(1,2,6);
+        }
+    }
+
+    document.getElementById('square7').onclick = function() {
+        if (isFree(2,0)) {
+            drawSymbol(2,0,7);
+        }
+    }
+
+    document.getElementById('square8').onclick = function() {
+        if (isFree(2,1)) {
+            drawSymbol(2,1,8);
+        }
+    }
+
+    document.getElementById('square9').onclick = function() {
+        if (isFree(2,2)) {
+            drawSymbol(2,2,9);
+        }
+    }
 }
 
 init();
